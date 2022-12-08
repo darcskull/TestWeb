054bb385f95f01af1f6d7c9b539dc821894731bb
darcskull <darcskul@gmail.com>
2022-12-08 08:33:04 +0200

    init BE

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import jdk.internal.net.http.common.Pair;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
@@ -8,9 +9,25 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class DemoApplication {
 
-	@GetMapping("/")
-	String home() {
-		return "Spring is here!";
+	@GetMapping("/calculate")
+	public Pair<Boolean, Integer> CalculateValue(
+			@RequestParam(value ="firstNumber") String firstNumber,
+			@RequestParam(value="secondNumber") String secondNumber) {
+		try {
+			int firstValue = Integer.parseInt(firstNumber);
+			int secondValue = Integer.parseInt(secondNumber);
+
+			if ( secondValue != 0){
+				return new Pair<>(true, firstValue/secondValue);
+			}
+			else {
+				return new Pair<>(false, 0);
+			}
+
+		}
+		catch (Exception exception){
+			return new Pair<>(false, 1);
+		}
 	}
 
 	public static void main(String[] args) {
