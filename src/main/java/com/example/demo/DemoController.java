610c5a9e2f7e079bccca4be74c5b640a0455cbdc
darcskull <darcskul@gmail.com>
2022-12-09 15:50:30 +0200

    add selenium tests

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    @CrossOrigin
     @GetMapping("/calculate")
     public ResponseEntity<String> CalculateValue(
             @RequestParam(value = "firstNumber") String firstNumber,
---
+++ src/test/java/com/example/demo/SeleniumTesting.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+
+public class SeleniumTesting {
+
+    @Test
+    public void testPositiveNumbers() throws InterruptedException {
+        testSelenium("12556", "55", String.valueOf(12556/55));
+    }
+
+    @Test
+    public void testDevideByZero() throws InterruptedException {
+        String zeroMessage = "Делене на -0- не е разрешена операция";
+        testSelenium("12556", "0", zeroMessage);
+    }
+
+    @Test
+    public void testInvalidFields() throws InterruptedException {
+        String invalidMessage = "Невалидни числени стойности";
+        testSelenium("5@34", "23!", invalidMessage);
+    }
+
+    public void testSelenium(String inputValue1, String inputValue2,
+                             String expectedValue) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        driver.get("http://localhost:8888/");
+        WebElement textBox1 = driver.findElement(By.id("input-01"));
+        textBox1.sendKeys(inputValue1);
+        WebElement textBox2 = driver.findElement(By.id("input-02"));
+        textBox2.sendKeys(inputValue2);
+        Thread.sleep(4600);
+        driver.findElement(By.cssSelector("button")).click();
+        Thread.sleep(4600);
+        String value = driver.findElement(By.id("input-03")).getAttribute("value");
+        Assert.assertEquals(expectedValue, value);
+        driver.quit();
+    }
+
+}
